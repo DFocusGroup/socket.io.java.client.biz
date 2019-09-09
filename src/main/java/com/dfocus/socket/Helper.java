8fe5d965f5816e77f6e74f9016b391225258a697
Howard.Zuo <leftstick@qq.com>
2019-09-09 22:58:47 +0800

    fix: update url helper

--- src/main/java/com/dfocus/socket/Helper.java
+++ src/main/java/com/dfocus/socket/Helper.java
@@ -2,9 +2,15 @@ package com.dfocus.socket;
 
 public class Helper {
 
-    public static String toUrl(String ...paths) {
-       return String.join("/", paths).replaceAll("/+", "/").replaceAll("^\\/", "").replaceAll("\\/$", "").replace("http:/", "http://")
-       .replace("https:/", "https://");
+    public static String toUrl(String... paths) {
+        StringBuffer sb = new StringBuffer();
+
+        for (String path : paths) {
+            sb.append(path);
+            sb.append("/");
+        }
+        return sb.toString().replaceAll("/+", "/").replaceAll("^\\/", "").replaceAll("\\/$", "")
+                .replace("http:/", "http://").replace("https:/", "https://");
     }
 
 }
