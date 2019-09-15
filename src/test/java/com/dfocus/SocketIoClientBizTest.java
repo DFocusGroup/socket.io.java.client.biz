85dea2c391db538691b6b1c55315a1eadc4d2615
Howard.Zuo <leftstick@qq.com>
2019-09-15 12:25:13 +0800

    test: detail check for connect method

--- src/test/java/com/dfocus/SocketIoClientBizTest.java
+++ src/test/java/com/dfocus/SocketIoClientBizTest.java
@@ -9,8 +9,12 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 import com.dfocus.enums.BizEvent;
+import com.dfocus.enums.ClientState;
 import com.dfocus.error.LifecycleException;
 import com.dfocus.factories.SocketIOFactory;
 import com.dfocus.options.SocketOpts;
@@ -28,6 +32,10 @@ import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 public class SocketIoClientBizTest {
+
+	final static List<String> DISCONNECT_EVENTS = Arrays.asList(Socket.EVENT_CONNECT_ERROR,
+			Socket.EVENT_CONNECT_TIMEOUT, Socket.EVENT_DISCONNECT, Socket.EVENT_ERROR, Socket.EVENT_RECONNECT_ERROR,
+			Socket.EVENT_RECONNECT_FAILED);
 
 	@Test(expected = LifecycleException.class)
 	public void connectCannotBeCalledMultipleTimes() throws URISyntaxException, LifecycleException {
@@ -104,7 +112,7 @@ public class SocketIoClientBizTest {
 		JSONObject authData = Helper.toJSONObject("projectId", "fm", "token", "validtoken");
 
 		SocketIOFactory factory = mock(SocketIOFactory.class);
-		Socket socket = mock(Socket.class);
+		final Socket socket = mock(Socket.class);
 
 		SocketIoClientBiz biz = new SocketIoClientBiz(factory);
 
@@ -144,7 +152,21 @@ public class SocketIoClientBizTest {
 			}
 		});
 
+		verify(socket, times(1)).on(eq(Socket.EVENT_RECONNECT), any(Emitter.Listener.class));
+		verify(socket, times(1)).on(eq(Socket.EVENT_CONNECTING), any(Emitter.Listener.class));
+		verify(socket, times(1)).on(eq(Socket.EVENT_RECONNECTING), any(Emitter.Listener.class));
+		verify(socket, times(2)).on(eq(Socket.EVENT_ERROR), any(Emitter.Listener.class));
+		verify(socket, times(1)).on(eq(Socket.EVENT_CONNECT), any(Emitter.Listener.class));
+		verify(socket, times(1)).emit(eq(BizEvent.AUTH.toString()), any(JSONObject.class), any(Ack.class));
 		verify(socket, times(1)).off(eq(Socket.EVENT_ERROR), any(Emitter.Listener.class));
+
+		DISCONNECT_EVENTS.forEach(new Consumer<String>() {
+			public void accept(String e) {
+				if (!Socket.EVENT_ERROR.equals(e)) {
+					verify(socket, times(1)).on(eq(e), any(Emitter.Listener.class));
+				}
+			}
+		});
 	}
 
 }
