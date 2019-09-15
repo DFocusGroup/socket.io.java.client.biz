e7b308f57b9be5e2db5dd1995e0d98b7fadaaf3a
Howard.Zuo <leftstick@qq.com>
2019-09-15 12:37:04 +0800

    test: add case for onStateChange

--- src/test/java/com/dfocus/SocketIoClientBizTest.java
+++ src/test/java/com/dfocus/SocketIoClientBizTest.java
@@ -20,6 +20,7 @@ import com.dfocus.factories.SocketIOFactory;
 import com.dfocus.options.SocketOpts;
 import com.dfocus.socket.Finish;
 import com.dfocus.socket.Helper;
+import com.dfocus.socket.StateChangeCallback;
 
 import org.json.JSONObject;
 import org.junit.Assert;
@@ -167,6 +168,61 @@ public class SocketIoClientBizTest {
 				}
 			}
 		});
+	}
+
+	@Test
+	public void stateChange() throws URISyntaxException, LifecycleException {
+		JSONObject authData = Helper.toJSONObject("projectId", "fm", "token", "validtoken");
+
+		SocketIOFactory factory = mock(SocketIOFactory.class);
+		final Socket socket = mock(Socket.class);
+
+		SocketIoClientBiz biz = new SocketIoClientBiz(factory);
+
+		doAnswer(new Answer<Void>() {
+
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				Emitter.Listener callback = (Emitter.Listener)invocation.getArgument(1);
+				callback.call();
+				return null;
+			}
+			
+		}).when(socket)
+			.on(eq(Socket.EVENT_CONNECT), any(Emitter.Listener.class));
+
+		doAnswer(new Answer<Void>() {
+
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				Ack callback = (Ack)invocation.getArgument(2);
+				callback.call("auth_success");
+				return null;
+			}
+			
+		}).when(socket)
+			.emit(eq(BizEvent.AUTH.toString()), any(JSONObject.class), any(Ack.class));
+	 
+		
+		when(factory.get()).thenReturn(socket);
+		when(factory.getAuthData()).thenReturn(authData);
+		when(socket.connect()).thenReturn(null);
+
+		biz.onStateChange(new StateChangeCallback(){
+		
+			@Override
+			public void onChange(ClientState state) {
+				Assert.assertEquals(ClientState.CONNECTED, state);
+			}
+		});
+		
+
+		biz.connect(new Finish(){
+			@Override
+			public void onFinished(String errorMessage) {
+			}
+		});
+		
 	}
 
 }
