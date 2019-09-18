9d25235fd0db8554cc3938b5c0d6b9a28e15a673
Howard.Zuo <leftstick@qq.com>
2019-09-18 14:17:47 +0800

    refactor: format

--- src/main/java/com/dfocus/socket/Helper.java
+++ src/main/java/com/dfocus/socket/Helper.java
@@ -4,29 +4,31 @@ import org.json.JSONObject;
 
 public class Helper {
 
-    public static String toUrl(String... paths) {
-        StringBuffer sb = new StringBuffer();
+	public static String toUrl(String... paths) {
+		StringBuffer sb = new StringBuffer();
 
-        for (String path : paths) {
-            sb.append(path);
-            sb.append("/");
-        }
-        return sb.toString().replaceAll("/+", "/").replaceAll("^\\/", "").replaceAll("\\/$", "")
-                .replace("http:/", "http://").replace("https:/", "https://");
-    }
+		for (String path : paths) {
+			sb.append(path);
+			sb.append("/");
+		}
+		return sb.toString().replaceAll("/+", "/").replaceAll("^\\/", "").replaceAll("\\/$", "")
+				.replace("http:/", "http://").replace("https:/", "https://");
+	}
 
-    public static JSONObject toJSONObject(final String... data) {
+	public static JSONObject toJSONObject(final String... data) {
 
-        final JSONObject obj = new JSONObject();
+		final JSONObject obj = new JSONObject();
 
-        for (int i = 0; i < data.length; i = i + 2) {
-            try {
-                obj.put(data[i], data[i + 1]);
-            } catch (Exception e) {
+		for (int i = 0; i < data.length; i = i + 2) {
+			try {
+				obj.put(data[i], data[i + 1]);
+			}
+			catch (Exception e) {
+				System.out.println(data[i + 1] + " is invalid value for " + data[i]);
+			}
+		}
 
-            }
-        }
+		return obj;
+	}
 
-        return obj;
-    }
 }
--- src/test/java/com/dfocus/socket/HelperTest.java
+++ src/test/java/com/dfocus/socket/HelperTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class HelperTest {
+
 	@Test
 	public void toUrlWithNoSlashSections() {
 		Assert.assertEquals(Helper.toUrl("http://dfocus.com", "api", "socket"), "http://dfocus.com/api/socket");
